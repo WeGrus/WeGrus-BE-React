21cb5f95e50e518d16c1ec27b66a53be79c318f9
biinggala <49203064+biinggala@users.noreply.github.com>
2021-12-23 15:35:38 +0900

    리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-  import Welcome from "./routes/Welcome";
-  import Login from "./routes/Login";
-  import Signup from "./routes/Signup";
-  import Study from "./routes/Study";
-  import Page from "./routes/Page";
-  import About from "./routes/About";
-  import Group from "./routes/Group";
-  import CreatePage from "./routes/Page_Create.js";
-  import UpdatePage from "./routes/Page_Update.js";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Welcome from "./components/screens/Welcome";
+import Login from "./components/screens/Login";
+import Signup from "./components/screens/Signup";
+import Study from "./components/screens/Study";
+import Page from "./components/screens/Page";
+import About from "./components/screens/About";
+import Group from "./components/screens/Group";
+import CreatePage from "./components/screens/Page_Create.js";
+import UpdatePage from "./components/screens/Page_Update.js";
 
 function App() {
   return (
@@ -22,18 +21,22 @@ function App() {
           <Route path="/study" element={<Study />} />
           <Route path="/study/:pagenum" element={<Page />} />
           <Route path="/study/write/:userid" element={<CreatePage />} />
-          <Route path="/study/update/:pagenum/:userid" element={<UpdatePage />} />
+          <Route
+            path="/study/update/:pagenum/:userid"
+            element={<UpdatePage />}
+          />
 
           <Route path="/board" element={<Study />} />
           <Route path="/board/write/:userid" element={<CreatePage />} />
           <Route path="/board/:pagenum" element={<Page />} />
-          <Route path="/board/update/:pagenum/:userid" element={<UpdatePage />} />
+          <Route
+            path="/board/update/:pagenum/:userid"
+            element={<UpdatePage />}
+          />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-
-
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
   );
