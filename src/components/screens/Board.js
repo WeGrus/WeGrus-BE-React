6dae4f7313a01c86e42afd7e7d0b0ab8d0966d40
seung <dmdgpdi@naver.com>
2021-12-30 04:45:24 +0900

    레이아웃 수정
    side bar 컴포넌트 생성, 간단한 레이아웃

--- src/components/screens/Board.js
+++ src/components/screens/Board.js
@@ -1,18 +1,30 @@
 import * as React from "react";
-
 import { Outlet, Link } from "react-router-dom";
+import SideBar from '../sidebar/SideBar';
+import styles from "./Board.module.css";
 
 function Board() {
+  const arr = ["전체","정보 공유","프로젝트 모집","취미 톡방","질문/답변","자유게시판","건의사항"];
   return (
-    <div>
-      <h1>Board</h1>
-      <Link to="write/1234">create</Link>
-      <h2>list</h2>
-      <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
-        <Link to="1">page1</Link> | <Link to="2">page2</Link> |{" "}
-        <Link to="3">page3</Link> |{" "}
-      </nav>
-      <Outlet />
+    <div className={styles.container}>
+      <div className={styles.aside}>
+        <SideBar arr={arr}/>
+      </div>
+      <div className={styles.content}>
+        <h1>Board</h1>
+        <h2>list</h2>
+        <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
+          <Link to="1">page1</Link> | <Link to="2">page2</Link> |{" "}
+          <Link to="3">page3</Link> |{" "}
+        </nav>
+        <Outlet />
+        <Link to="write/1234">create</Link>
+      </div>
+
+      
+      
+      
+      
     </div>
   );
 }
--- src/components/screens/Group.js
+++ src/components/screens/Group.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 
+
 function Group() {
     return (
       <div>
+  
         <h1>group</h1>
       </div>
     );
