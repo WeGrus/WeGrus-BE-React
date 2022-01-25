f6b6c3ee7401c33c7fcdf56cc5a9b64eb33eefae
biinggala <49203064+biinggala@users.noreply.github.com>
2022-01-25 17:53:22 +0900

    Feat. 카카오 oauth 백엔드 방식 변경
    - 인가코드를 백엔드로 넘겨 주고 백엔드에서 resolve 하는 방식으로 변경
    - 이메일 인증 api 연결

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,27 +1,298 @@
 import * as React from "react";
-import { Outlet } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import PageTitle from "../shared/PageTitle";
+import Pagination from "../shared/Pagination";
+import PostBar from "../shared/PostBar";
+import ScreenTitle from "../shared/ScreenTitle";
 import SideBar from "../shared/SideBar";
+import img from "./../../images/Polygon.jpg";
 
 const Content = styled.div`
   width: 924px;
   background-color: white;
 `;
+const SearchBarSection = styled.div`
+  width: 910.07px;
+  margin: 0 auto;
+  height: 64px;
 
-const arr = [
-  { content: "내 정보", filter: "myinfo" },
-  { content: "내가 쓴 게시글", filter: "myposts" },
-  { content: "내가 쓴 댓글", filter: "mycomments" },
-  { content: "계정 설정", filter: "accountinfo" },
+  border-bottom: 2px solid #0b665c;
+  font-size: 14px;
+  display: flex;
+  flex-direction: row;
+`;
+const SearchBarForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  margin-left: 20px;
+  margin-top: 16px;
+`;
+const SearchBarSelect = styled.select`
+  width: 117px;
+  height: 32px;
+  padding-left: 8px;
+  border-radius: 50px;
+  line-height: 16.41px;
+  color: #c4c4c4;
+  border: 1px solid #c4c4c4;
+
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background: url(${img});
+  background-repeat: no-repeat;
+  background-size: 17px 17px;
+  background-position: bottom 8px right 11px;
+`;
+const SearchBar = styled.div`
+  margin-left: 9px;
+  position: relative;
+`;
+const SearchBarInput = styled.input`
+  min-width: 331.48px;
+  height: 31.59px;
+  border: 1px solid #c4c4c4;
+  border-radius: 50px;
+  padding-left: 19.87px;
+`;
+const SearchBarSubmit = styled.input`
+  position: absolute;
+  right: 3.71px;
+  top: 4.25px;
+  width: 28.9px;
+  height: 25.27px;
+  border: none;
+  border-radius: 50px;
+  border-color: #c4c4c4;
+  background-color: #c4c4c4;
+`;
+const SearchBarFilter = styled.select`
+  width: 60.52px;
+  height: 16.59px;
+  line-height: 16.41px;
+  margin-top: 24px;
+  margin-left: 100.52px;
+  border: none;
+
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+
+  background: url(${img});
+  background-repeat: no-repeat;
+  background-size: 12.35px 10.43px;
+  background-position: right 2px bottom 2px;
+`;
+const CreateBtnLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #0b665c;
+  width: 110px;
+  height: 33px;
+  border-radius: 16.5px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  margin-left: 100.32px;
+  margin-top: 15.85px;
+`;
+const InforBar = styled.div`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 16.41px;
+  width: 909.07px;
+  margin: 0 auto;
+  height: 34px;
+  border-bottom: 2px solid #0b665c;
+`;
+const InforContents = styled.div`
+  padding-top: 10px;
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+`;
+const Number = styled.div`
+  min-width: 65px;
+  text-align: center;
+  margin-left: 23px;
+`;
+const Categorization = styled.div`
+  width: 90px;
+  text-align: center;
+  margin-left: 4px;
+`;
+const Title = styled.div`
+  width: 373px;
+  margin-left: 31px;
+`;
+const Writer = styled.div`
+  width: 77px;
+  text-align: center;
+`;
+const Date = styled.div`
+  width: 63px;
+  text-align: center;
+  margin-left: 16px;
+`;
+const Hits = styled.div`
+  width: 40px;
+  text-align: center;
+  margin-left: 11px;
+`;
+const Recommendation = styled.div`
+  width: 40px;
+  text-align: center;
+  margin-left: 9px;
+`;
+const Comment = styled.div`
+  width: 40px;
+  text-align: center;
+  margin-left: 7px;
+`;
+const PostInforBar = styled.div`
+  width: 909.07px;
+  height: 31px;
+  margin: 0 auto;
+  font-size: 14px;
+  line-height: 16.41px;
+  border-bottom: 1px solid black;
+`;
+const PostCotent = styled.div`
+  padding-top: 8px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const subCategory = [
+  { content: "내 정보", filter: "내 정보" },
+  { content: "내가 쓴 게시글", filter: "내가 쓴 게시글" },
+  { content: "내가 쓴 댓글", filter: "내가 쓴 댓글" },
+  { content: "계정 설정", filter: "계정 설정" },
+];
+const postData = [
+  {
+    number: 1,
+    categorization: "내 정보",
+    title: "test",
+    writer: "17 김승태",
+    date: "2022/01/19",
+    hits: 156,
+    recommend: 51,
+    comment: 53,
+  },
 ];
 
+const selectDate = ["추천순", "댓글순", "조회순"];
+
 function Profile() {
+  const input = useLocation();
+
+  let subBarTarget; // 페이지에서 뒤로가기를 누르거나 목록을 누를 시 즉 subCategory
+
+  if (input.state != null) {
+    subBarTarget = input.state.category;
+  } else {
+    subBarTarget = "내 정보";
+  }
+
+  const [target, setTarget] = React.useState(subBarTarget); // 게시판중 사이드바와 분류를 나눔. 즉 subCategory
+  const [posts, setPosts] = React.useState(
+    postData.filter((data) => data.categorization === target)
+  );
+  const [text, setText] = React.useState();
+  const [limit, setLimit] = React.useState(19);
+  const [page, setPage] = React.useState(1);
+  const [selected, setSelected] = React.useState("");
+  const offset = (page - 1) * limit;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  const handleSearchBarFilter = (e) => {
+    setSelected(e.target.value);
+  };
+
+  React.useEffect(() => {
+    // 서브바에서 필터가 바뀌면 값을 변환.
+    console.log(selected);
+  }, [selected]);
+
+  React.useEffect(() => {
+    // 서브바에서 필터가 바뀌면 값을 변환.
+    let test = [...postData.filter((data) => data.categorization === target)];
+    setPosts([...test]);
+    setPage(1);
+  }, [target]);
+
   return (
     <>
       <PageTitle title="프로필" />
-      <SideBar posts={arr} />
+      <SideBar posts={subCategory} getFilter={setTarget}></SideBar>
       <Content>
+        <ScreenTitle>{`프로필 | ${target}`}</ScreenTitle>
+        <SearchBarSection>
+          <SearchBarForm onSubmit={handleSubmit(onSubmit)}>
+            <SearchBarSelect {...register("option")}>
+              <option>제목 + 내용</option>
+              <option>제목</option>
+              <option>작성자</option>
+            </SearchBarSelect>
+            <SearchBar>
+              <SearchBarInput
+                {...register("exampleRequired", { required: true })}
+              />
+              <SearchBarSubmit type="submit" value="" />
+            </SearchBar>
+          </SearchBarForm>
+
+          <SearchBarFilter onChange={handleSearchBarFilter} value={selected}>
+            {selectDate.map((value) => (
+              <option value={value} key={value}>
+                {value}
+              </option>
+            ))}
+            {/* <option value="추천">추천순</option>
+            <option value="조회">조회순</option>
+            <option value="댓글">댓글순</option> */}
+          </SearchBarFilter>
+
+          <CreateBtnLink
+            to="/board/write/1234"
+            state={{ category: "프로필", subCategory: target }}
+          >
+            create
+          </CreateBtnLink>
+        </SearchBarSection>
+
+        <InforBar>
+          <InforContents>
+            <Number>번호</Number>
+            <Categorization>분류</Categorization>
+            <Title>제목</Title>
+            <Writer>작성자</Writer>
+            <Date>작성일자</Date>
+            <Hits>조회</Hits>
+            <Recommendation>추천</Recommendation>
+            <Comment>댓글</Comment>
+          </InforContents>
+        </InforBar>
+
+        <PostBar target={target} page={page} data={posts} />
+
+        <Pagination
+          total={posts.length}
+          limit={limit}
+          page={page}
+          setPage={setPage}
+        />
         <Outlet />
       </Content>
     </>
--- src/components/screens/UserInfo.js
+++ src/components/screens/UserInfo.js
@@ -4,40 +4,136 @@ import Button from "../auth/Button";
 import HeaderContainer from "../auth/HeaderContainer";
 import PageTitle from "../shared/PageTitle";
 import Input from "../auth/Input";
-import FormBox from "../auth/FormBox";
 import axios from "axios";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import { API_HOST } from "../../App";
 
-const InhaEmail = styled.div`
+const DEPARTMENTS = [
+  "기계공학과",
+  "항공우주공학과",
+  "조선해양공학과",
+  "산업경영공학과",
+  "화학공학과",
+  "생명공학과",
+  "고분자공학과",
+  "신소재공학과",
+  "사회인프라공학과",
+  "환경공학과",
+  "공간정보공학과",
+  "건축학부",
+  "에너지자원공학과",
+  "전기공학과",
+  "전자공학과",
+  "정보통신공학과",
+  "수학과",
+  "통계학과",
+  "물리학과",
+  "화학과",
+  "생명과학과",
+  "해양과학과",
+  "식품영양학과",
+  "경영학과",
+  "글로벌금융학과",
+  "아태물류학부",
+  "국제통상학과",
+  "국어교육과",
+  "영어교육과",
+  "사회교육과",
+  "체육교육과",
+  "교육학과",
+  "수학교육과",
+  "행정학과",
+  "정치외교학과",
+  "경제학과",
+  "소비자학과",
+  "아동심리학과",
+  "사회복지학과",
+  "미디어커뮤니케이션학과",
+  "한국어문학과",
+  "사학과",
+  "철학과",
+  "중국학과",
+  "일본언어문화학과",
+  "영어영문학과",
+  "프랑스언어문화학과",
+  "문화콘텐츠문화경영학과",
+  "의예과",
+  "간호학과",
+  "의학과",
+  "소프트웨어융합공학과",
+  "산업경영학과",
+  "메카트로닉스공학과",
+  "금융투자학과",
+  "IBT학과",
+  "ISE학과",
+  "KLC학과",
+  "컴퓨터공학과",
+  "인공지능공학과",
+  "데이터사이언스학과",
+  "스마트모빌리티공학과",
+  "디자인테크놀로지학과",
+  "소프트웨어융합대학(SCSC)",
+  "조형예술학과",
+  "디자인융합학과",
+  "연극영화학과",
+  "의류디자인학과",
+];
+
+const LoginForm = styled.form`
+  margin-bottom: 60px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SSelect = styled.select`
   width: 292px;
   padding: 10px;
   border-radius: 18px;
   background-color: #f5f5f5;
   margin-bottom: 8px;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  span {
-    color: gray;
-  }
+  border: none;
 `;
 
-const LoginForm = styled.form`
-  margin-bottom: 60px;
-`;
+const Select = React.forwardRef(
+  ({ onChange, name, options, placeholder }, ref) => (
+    <>
+      <SSelect name={name} ref={ref} onChange={onChange}>
+        <option value="hide">*-- {placeholder} --</option>
+        {options.map((value) => (
+          <option key={value} value={value}>
+            {value}
+          </option>
+        ))}
+      </SSelect>
+    </>
+  )
+);
 
 function EmailAuth() {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, formState } = useForm();
 
   const onSubmit = (data) => {
-    const Id = data.schoolId;
-    console.log(Id);
+    data.email = `${data.email}@inha.edu`;
+    data.phone = data.phone
+      .replace(/[^0-9]/, "")
+      .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+
+    const body = {
+      academicStatus: data.academicStatus,
+      department: data.department,
+      email: data.email,
+      grade: data.grade,
+      name: data.name,
+      phone: data.phone,
+    };
+    console.log(body);
     axios
-      .post(
-        `http://ec2-3-35-129-82.ap-northeast-2.compute.amazonaws.com:8080/signup/check/email?email=${Id}%40inha.edu`
-      )
+      .post(`${API_HOST}signup`, {
+        headers: { "Content-Type": "application/json" },
+        body,
+      })
       .then((res) => console.log(res))
       .catch((res) => console.log(res));
   };
@@ -51,16 +147,39 @@ function EmailAuth() {
         <span>인하대학교 학생 이메일을 통해 인증을 완료해 주세요.</span>
       </HeaderContainer>
       <LoginForm onSubmit={handleSubmit(onSubmit)}>
-        <InhaEmail>
-          <input
-            {...register("schoolId", {
-              required: "학번을 입력하세요.",
-            })}
-            placeholder="학번 입력"
-            type="text"
-          />
-          <span>@inha.edu</span>
-        </InhaEmail>
+        <Input
+          {...register("name", {
+            required: "name is required.",
+          })}
+          type="text"
+          placeholder="*이름"
+          hasError={Boolean(formState.errors?.name?.message)}
+        />
+
+        <Select
+          {...register("department", { required: "department is required." })}
+          placeholder="학과"
+          options={DEPARTMENTS}
+        />
+        <Select
+          {...register("grade", { required: "grade is required." })}
+          placeholder="학년"
+          options={["1학년", "2학년", "3학년", "4학년", "그 외"]}
+        />
+        <Select
+          {...register("academicStatus", {
+            required: "academicStatus is required.",
+          })}
+          placeholder="재학 여부"
+          options={["재학", "휴학", "졸업"]}
+        />
+
+        <Input
+          {...register("phone")}
+          type="tel"
+          placeholder="전화번호"
+          hasError={Boolean(formState.errors?.password?.message)}
+        />
 
         <Button color="#106557" ftcolor="white" type="submit" />
       </LoginForm>
