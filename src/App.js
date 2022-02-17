b1b3a795eb14e10f251b95af5325189b0b78e534
seung <dmdgpdi@naver.com>
2022-02-18 01:56:44 +0900

    ETC: 베이스 url 변경

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ import Signup from "./components/screens/Signup";
 import { actionCreators } from "./store";
 import jwt_decode from "jwt-decode";
 
-axios.defaults.baseURL =
-  "http://ec2-3-35-129-82.ap-northeast-2.compute.amazonaws.com:8080/";
+axios.defaults.baseURL = "http://api.igrus.net:8080/"
+  //"http://ec2-3-35-129-82.ap-northeast-2.compute.amazonaws.com:8080/";
 
 //axios에서 baseURL을 지정해서 반복하는 코드를 없애는 것입니다. 이것때문에 기능이 안되신다면 말씀해주세요.
 
