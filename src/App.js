69ab31329ebec7c83a73a29dc43d8ad52acec815
seung <dmdgpdi@naver.com>
2022-02-20 18:21:29 +0900

    Fix: 정상화

--- src/App.js
+++ src/App.js
@@ -194,63 +194,57 @@ function App(props) {
       <BrowserRouter>
         <GlobalStyles />
         <Routes>
-          {(checkRender === true)?
-                    <Route path="/" element={<Layout />}>    
-                    {role !== null ? (
-                      <>
-                        <Route path="/announce/:boardId/:page/:sorted/:isSearch" element={<Announce />} />
-                        <Route path="/announce/:pagenum" element={<NewPage />} />
-                        <Route path="/announce/write/:userid" element={<CreatePage />}/>
-                        <Route path="/announce/update/:pagenum/:userid" element={<NewUpdatePage />}/>
-                        
-                        {(joinPermission !== null && joinPermission?.length !== 0) ||
-                        isJoinGroup ? (
-                          <>
-                            <Route path="/group/:boardId/:page/:sorted/:isSearch" element={<Group />} />
-                            <Route path="/group/:pagenum" element={<NewPage />} />
-                            <Route
-                              path="/group/write/:userid"
-                              element={<CreatePage />}
-                            />
-                            <Route
-                              path="/group/update/:pagenum/:userid"
-                              element={<NewUpdatePage />}
-                            />
-                          </>
-                        ) : null}
-        
-                        <Route path="/study/:boardId/:page/:sorted/:isSearch" element={<Study />} />
-                        <Route path="/study/:pagenum" element={<NewPage />} />
-                        <Route path="/study/write/:userid" element={<CreatePage />} />
-                        <Route path="/study/update/:pagenum/:userid" element={<NewUpdatePage />} />
-        
-                        <Route path="/board/:boardId/:page/:sorted/:isSearch" element={<Board />} />
-                        <Route path="/board/write/:userid" element={<CreatePage />} />
-                        <Route path="/board/:pagenum" element={<NewPage />} />
-                        <Route path="/board/update/:pagenum/:userid" element={<NewUpdatePage />} />
-                        <Route path="/profile" element={<Profile />} />
-        
-                       
-        
-                        <>
-                          {isAuthority === true ? (
-                            <Route path="/operator" element={<Operator />} />
-                          ) : null}
-                        </>
-                        <Route path="/" element={<NewPage />} />
-                        {/* <Route path="/" element={<About />} /> */}
-                      </>
-                    ) : (
-                    <>
-                    <Route path="/" element={<Profile />} />
-                    {/* <Route path="/" element={<About />} /> */}
-                    </>
-                    )}
-                    
-                  </Route>
-          :
-          null
-          }
+          <Route path="/" element={<Layout />}>
+            {role !== null ? (
+              <>
+                <Route path="/announce/:boardId/:page/:sorted/:isSearch" element={<Announce />} />
+                <Route path="/announce/:pagenum" element={<NewPage />} />
+                <Route path="/announce/write/:userid" element={<CreatePage />} />
+                <Route path="/announce/update/:pagenum/:userid" element={<NewUpdatePage />} />
+
+                {(joinPermission !== null && joinPermission?.length !== 0) ||
+                  isJoinGroup ? (
+                  <>
+                    <Route path="/group/:boardId/:page/:sorted/:isSearch" element={<Group />} />
+                    <Route path="/group/:pagenum" element={<NewPage />} />
+                    <Route
+                      path="/group/write/:userid"
+                      element={<CreatePage />}
+                    />
+                    <Route
+                      path="/group/update/:pagenum/:userid"
+                      element={<NewUpdatePage />}
+                    />
+                  </>
+                ) : null}
+
+                <Route path="/study/:boardId/:page/:sorted/:isSearch" element={<Study />} />
+                <Route path="/study/:pagenum" element={<NewPage />} />
+                <Route path="/study/write/:userid" element={<CreatePage />} />
+                <Route path="/study/update/:pagenum/:userid" element={<NewUpdatePage />} />
+
+                <Route path="/board/:boardId/:page/:sorted/:isSearch" element={<Board />} />
+                <Route path="/board/write/:userid" element={<CreatePage />} />
+                <Route path="/board/:pagenum" element={<NewPage />} />
+                <Route path="/board/update/:pagenum/:userid" element={<NewUpdatePage />} />
+                <Route path="/profile" element={<Profile />} />
+
+
+
+                <>
+                  {isAuthority === true ? (
+                    <Route path="/operator" element={<Operator />} />
+                  ) : null}
+                </>
+                <Route path="/" element={<About />} />
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<About />} />
+              </>
+            )}
+
+          </Route>
           <Route path="/login" element={<Login />} />
           {!authenticated ? (
             <>
