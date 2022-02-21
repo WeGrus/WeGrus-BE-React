f818618700664d4b2c15c702f6135e9bb0b40fb1
seung <dmdgpdi@naver.com>
2022-02-22 01:43:02 +0900

    Revert "update oauth.js"
    This reverts commit 728e7f0844d6fcbca95fad23d50ed27d5cec071e.

--- src/components/auth/OAuth.js
+++ src/components/auth/OAuth.js
@@ -45,7 +45,7 @@ function OAuth(props) {
         },
         withCredentials: true,
       }) //로그인 api로 인가코드를 보내 백엔드에서 카카오 로그인 완료
-      .then(async (res) => {
+      .then((res) => {
         const KAKAO_ID = res.data.data.userId;
         const RESULT = res.data.data.status;
 
@@ -57,7 +57,7 @@ function OAuth(props) {
           navigate("/login/email-auth");
         } else if ("success") {
           const ACCESS_TOKEN = res.data.data.accessToken;
-          await props.loginSuccess(ACCESS_TOKEN);
+          props.loginSuccess(ACCESS_TOKEN);
 
           //console.log(ACCESS_TOKEN);
           //서버에서 쿠키로 전송한 refresh_token을 확인하는 코드입니다
