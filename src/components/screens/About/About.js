2e82a673e03bf4c946c572b322cfe2fcb5f53957
biinggala <49203064+biinggala@users.noreply.github.com>
2022-02-23 21:05:38 +0900

    Update About.js

--- src/components/screens/About/About.js
+++ src/components/screens/About/About.js
@@ -211,7 +211,7 @@ function About(props) {
           <BigTitle>
             <Emoji>🙋🏻</Emoji> IGRUS는 어떤 동아리인가요?
           </BigTitle>
-          {props?.userReducer?.roles === null &&
+          {props?.userReducer?.roles === "ROLE_GUEST" &&
           props?.userReducer?.authenticated ? (
             <BtnLink onClick={handleEnrollClub}>동아리 가입 신청</BtnLink>
           ) : null}
